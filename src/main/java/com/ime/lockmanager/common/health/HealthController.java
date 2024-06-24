db7941e58cef125e4a6b6bf508920e37632453da
jaepyo99 <jaepyo99@naver.com>
2024-06-24 23:24:46 +0900

    feat: 헬스체크 컨트롤러 추가

---
+++ src/main/java/com/ime/lockmanager/common/health/HealthController.java
@@ -0,0 +1,12 @@
+package com.ime.lockmanager.common.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health-check")
+    public String healthChech(){
+        return "health-check";
+    }
+}
